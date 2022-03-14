ccf28cd8e12b54b7e0f2ff9f145495616f1aab41
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-14 11:07:41 -0500

    Feat: Call locations and species

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -6,8 +6,14 @@ function Characters({characters = []}) {
         {
             characters.map((item, index)=> (
                 <div key={index} className='col'>
-                    <div className='card' >
+                    <div className='card' style={{minWidth:"150px"}}>
                         <img src={item.image} alt="" />
+                        <div className='card-body' >
+                            <h5 className='card-title' >{item.name} </h5>
+                            <hr />
+                            <p>species: {item.species}</p>
+                            <p>location: {item.location.name}</p>
+                        </div>
                     </div>
                 </div>
             ))
