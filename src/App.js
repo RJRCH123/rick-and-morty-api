8c289b1eabb87352613e094715487d5024105491
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-14 10:03:02 -0500

    Build: Add bootstrap links in html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-uppercase" href="/">Rick and Morty Api</a>
+      </div>
+    </nav>
   );
 }
 
